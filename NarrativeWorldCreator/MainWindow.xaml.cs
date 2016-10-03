af389bae122f5162b5566be26e8b8fa782e4508b
Marijn Goedegebure <Marijn Goedegebure>
2016-10-03 16:59:39 +0200

    Initial commit, with file selector.

---
+++ NarrativeWorldCreator/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+using QuickGraph;
+
+namespace NarrativeWorldCreator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var edges = new SEdge<int>[] { new SEdge<int>(1, 2), new SEdge<int>(0, 1) };
+            var graph = edges.ToAdjacencyGraph<int, SEdge<int>>(false);
+        }
+
+        private void btnOpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+                txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
+        }
+    }
+}
