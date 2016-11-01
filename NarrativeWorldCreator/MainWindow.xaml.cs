f3e58bd60c58af59949f45b0f89137599b28b403
Marijn Goedegebure <marijngoedegebure@gmail.com>
2016-11-01 17:25:22 +0100

    Added DatabaseSearch connection with Entika database.

--- NarrativeWorldCreator/MainWindow.xaml.cs
+++ NarrativeWorldCreator/MainWindow.xaml.cs
@@ -4,6 +4,8 @@ using System.Windows;
 using Microsoft.Win32;
 using NarrativeWorldCreator.Pages;
 using Semantics.Data;
+using System.Collections.Generic;
+using Semantics.Entities;
 
 namespace NarrativeWorldCreator
 {
@@ -16,10 +18,12 @@ namespace NarrativeWorldCreator
         {
             InitializeComponent();
             _mainFrame.Navigate(new InitPage());
-            //Database.Initialize();
-            //Database.LoadProject("C://Users//marij//Documents//Master Thesis own folder//Entika test databases//Test.edp");
-            //var names = Database.Current.SelectAll<String>(LocalizationTables.NodeName);
-            //Database.Current.SelectAll<String>(3, LocalizationTables.NodeName, Columns.Name);
+            Database.Initialize();
+            Database.LoadProject("C://Users//marij//Documents//Master Thesis own folder//Entika test databases//gamesimple.edp");
+            var names = Database.Current.SelectAll<String>(LocalizationTables.NodeName);
+            Database.Current.SelectAll<String>(3, LocalizationTables.NodeName, Columns.Name);
+            List<PhysicalEntity> allPhysicalEntities = DatabaseSearch.GetNodes<PhysicalEntity>(true);
+            List<PhysicalObject> allPhysicalObjects = DatabaseSearch.GetNodes<PhysicalObject>(true);
         }
     }
 }
