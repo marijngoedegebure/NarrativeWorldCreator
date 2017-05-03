4423a0e5a085f8796e817f31ec557f1e16a95d6a
Marijn Goedegebure <marijngoedegebure@gmail.com>
2017-05-03 09:44:58 +0200

    Small adjustments to placementsolver, now able to use arrays of structs.

--- NarrativeWorldCreator/Solvers/PlacementSolverGPU.cs
+++ NarrativeWorldCreator/Solvers/PlacementSolverGPU.cs
@@ -94,56 +94,5 @@ namespace NarrativeWorldCreator.Solvers
             }
 
         }
-
-        private const int Length = 1000000;
-
-        [GpuManaged]
-        public static void Transform(Complex[] result, Complex[] arg1, Complex[] arg2)
-        {
-            var lp = new LaunchParam(16, 256);
-            // Gpu.Default.For(0, result.Length, action);
-            Gpu.Default.Launch(Kernel, lp, result, arg1, arg2);
-        }
-
-        public static void Kernel(Complex[] result, Complex[] arg1, Complex[] arg2)
-        {
-            var start = blockIdx.x * blockDim.x + threadIdx.x;
-            var stride = gridDim.x * blockDim.x;
-            for (var i = start; i < result.Length; i += stride)
-            {
-                result[i] = new Complex
-                {
-                    Real = arg1[i].Real + arg2[i].Real,
-                    Imag = arg1[i].Imag + arg2[i].Imag
-                };
-            }
-        }
-
-        private static T ConvertValue<T, TU>(TU value) where TU : IConvertible
-        {
-            return (T)Convert.ChangeType(value, typeof(T));
-        }
-
-        public struct Complex
-        {
-            public double Real;
-            public double Imag;
-
-            public override string ToString()
-            {
-                return $"({Real}+I{Imag})";
-            }
-        }
-
-        [GpuManaged]
-        public static void AddComplexDouble()
-        {
-            var rng = new Random();
-            var arg1 = Enumerable.Range(0, Length).Select(i => new Complex { Real = rng.NextDouble(), Imag = rng.NextDouble() }).ToArray();
-            var arg2 = Enumerable.Range(0, Length).Select(i => new Complex { Real = rng.NextDouble(), Imag = rng.NextDouble() }).ToArray();
-            var result = new Complex[Length];
-
-            Transform(result, arg1, arg2);
-        }
     }
 }
