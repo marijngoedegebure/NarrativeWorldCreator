e8ae835a20ddbe4c0a929f4f6662e4dca22d370e
Marijn Goedegebure <marijngoedegebure@gmail.com>
2017-07-18 15:27:17 +0200

    Added delta models

---
+++ NarrativeWorldCreator/Models/NarrativeRegionFill/InstanceDelta.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NarrativeWorldCreator.Models.NarrativeRegionFill
+{
+    internal class InstanceDelta
+    {
+        internal InstanceDeltaType DT;
+
+        internal enum InstanceDeltaType
+        {
+            Add = 0,
+            Remove = 1,
+            Move = 2
+        }
+
+        EntikaInstance RelatedInstance;
+
+        Vector3 Position;
+
+        public InstanceDelta(EntikaInstance inst, InstanceDeltaType deltaType, Vector3? pos)
+        {
+            this.DT = deltaType;
+            this.RelatedInstance = inst;
+            this.Position = pos.GetValueOrDefault();
+        }
+    }
+}
---
+++ NarrativeWorldCreator/Models/NarrativeRegionFill/RelationshipDelta.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NarrativeWorldCreator.Models.NarrativeRegionFill
+{
+    internal class RelationshipDelta
+    {
+        internal RelationshipDeltaType DT;
+
+        internal enum RelationshipDeltaType
+        {
+            Add = 0,
+            Remove = 1
+        }
+
+        RelationshipInstance RelatedInstance;
+
+        public RelationshipDelta(RelationshipInstance inst, RelationshipDeltaType deltaType)
+        {
+            this.DT = deltaType;
+            this.RelatedInstance = inst;
+        }
+    }
+}
